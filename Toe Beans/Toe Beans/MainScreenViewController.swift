63267e382abe7c1905e46afd69c534e27216ddfe
Alyssa Ma <Alyssa.Ma80@myhunter.cuny.edu>
2021-04-14 20:11:52 -0400

    initial commit

---
+++ Toe Beans/Toe Beans/MainScreenViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Toe Beans
+//
+//  Created by Alyssa Ma on 4/14/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
