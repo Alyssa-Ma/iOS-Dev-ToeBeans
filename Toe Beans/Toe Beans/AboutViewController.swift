44cd846174f7109aeaab716831f15b5ce933cf22
Alyssa Ma <Alyssa.Ma80@myhunter.cuny.edu>
2021-04-19 19:02:04 -0400

    about screen and close button done

---
+++ Toe Beans/Toe Beans/AboutViewController.swift
@@ -0,0 +1,28 @@
+//
+//  AboutViewController.swift
+//  Toe Beans
+//
+//  Created by Alyssa Ma on 4/19/21.
+//
+
+import UIKit
+
+class AboutViewController: UIViewController {
+    //close button
+    @IBAction func close()
+    {
+        dismiss(animated: true, completion: nil)
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
