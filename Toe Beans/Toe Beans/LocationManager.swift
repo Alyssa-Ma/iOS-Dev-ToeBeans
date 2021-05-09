124b1b375529c33d8cf26e0933337e6d6afbf663
Alyssa Ma <Alyssa.Ma80@myhunter.cuny.edu>
2021-05-09 02:44:46 -0400

    finished converting search text to coords

--- Toe Beans/Toe Beans/LocationManager.swift
+++ Toe Beans/Toe Beans/LocationManager.swift
@@ -8,15 +8,49 @@
 import Foundation
 import CoreLocation
 
+//name of location and coords
 struct Location {
-    let coordinates: CLLocationCoordinate2D
+    let title: String
+    let coordinates: CLLocationCoordinate2D?
 }
+
 class LocationManager: NSObject {
     static let shared = LocationManager()
     
-    let manager = CLLocationManager()
-    
+    //geocode string location to coords
     public func findLocations(with query: String, completion: @escaping (([Location]) -> Void)) {
+        let geoCoder = CLGeocoder()
         
+        //handle error
+        geoCoder.geocodeAddressString(query) {places, error in
+            guard let places = places, error == nil else {
+                completion([])
+                return
+            }
+            
+            let models: [Location] = places.compactMap({ place in
+                var name = ""
+                if let locationName = place.name {
+                    name += locationName
+                }
+                
+                if let adminRegion = place.administrativeArea {
+                    name += ", \(adminRegion)"
+                }
+                
+                if let locality = place.locality {
+                    name += ", \(locality)"
+                }
+                if let country = place.country {
+                    name += ", \(country)"
+                }
+                
+                print("place string test \(place)")
+                
+                let result = Location(title: name, coordinates: place.location?.coordinate)
+                return result
+            })
+            completion(models)
+        }
     }
 }
