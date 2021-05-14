7b53f539ca255d832fc581d115b60b2a39e9f833
Alyssa Ma <Alyssa.Ma80@myhunter.cuny.edu>
2021-05-14 15:56:58 -0400

    favorite view navbar

--- Toe Beans/Toe Beans/FavoriteTableViewController.swift
+++ Toe Beans/Toe Beans/FavoriteTableViewController.swift
@@ -22,7 +22,7 @@ class FavoriteTableViewController: UITableViewController {
     // MARK: - Table view data source
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return 5
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
