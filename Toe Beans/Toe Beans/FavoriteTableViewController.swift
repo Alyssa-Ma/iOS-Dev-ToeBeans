e5570abfdfb78f4273f04619d4527facb96d9eb8
Alyssa Ma <Alyssa.Ma80@myhunter.cuny.edu>
2021-05-14 13:17:24 -0400

    favorite table view

--- Toe Beans/Toe Beans/FavoriteTableViewController.swift
+++ Toe Beans/Toe Beans/FavoriteTableViewController.swift
@@ -21,14 +21,13 @@ class FavoriteTableViewController: UITableViewController {
 
     // MARK: - Table view data source
 
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
-
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "FavoriteItem", for: indexPath)
+        return cell
     }
     
     // MARK:- IB Actions
